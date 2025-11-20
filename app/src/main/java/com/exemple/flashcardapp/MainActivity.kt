e13fb7c8be38387166c2136a8f12964e32ef60b8
unknown <woodringjuniorgraphique@gmail.com>
2025-11-20 16:44:49 -0500

    ChangebuttomColor

--- app/src/main/java/com/exemple/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/exemple/flashcardapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.exemple.flashcardapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val flashcard_answer2 = findViewById<TextView>(R.id.flashcard_answer2)
+        val flashcardAnswer3 = findViewById<TextView>(R.id.flashcard_answer3)
+
+
+        flashcardAnswer.setOnClickListener {
+            flashcardAnswer.setBackgroundColor(getResources().getColor(R.color.Rouge, null))
+            flashcard_answer2.setBackgroundColor(getResources().getColor(R.color.vert, null))
         }
+
+        flashcard_answer2.setOnClickListener {
+            flashcard_answer2.setBackgroundColor(getResources().getColor(R.color.vert, null))
+        }
+
+        flashcardAnswer3.setOnClickListener {
+            flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.Rouge, null))
+            flashcard_answer2.setBackgroundColor(getResources().getColor(R.color.vert, null))
+        }
+
+       }
     }
-}
